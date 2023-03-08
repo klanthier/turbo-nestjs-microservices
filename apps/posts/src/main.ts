8aef9fa4bedf6a533570514c0a16b70498cb0f54
Kevin Lanthier <info@kevinlanthier.me>
2023-03-07 19:08:03 -0500

    🎉Initial setup with shared configs across services
    * Eslint
    * Typescript
    * Two "service" type running
    
    Tests are all auto-generated from nest for now and will need to be revisited

---
+++ apps/posts/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3001);
+}
+
+// Only disable this for our entry point -- Needed for commonjs module resolution
+// eslint-disable-next-line unicorn/prefer-top-level-await
+bootstrap();
